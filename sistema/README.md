f772e5920457c1f4f710440c35280dfedfd0afca
LHAnccoC <lhanccoc@unjbg.edu.pe>
2025-12-16 16:28:47 -0500

    Mover archivos a la carpeta 'sistema'

---
+++ sistema/README.md
@@ -0,0 +1,2 @@
+# SO-PU02-SistemaXV6
+Interacción con un sistema operativo real de tipo UNIX (XV6)
---
+++ sistema/syscall.c
@@ -0,0 +1,290 @@
+#include "types.h"
+#include "defs.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+#include "x86.h"
+#include "syscall.h"
+#include "stat.h"
+
+// User code makes a system call with INT T_SYSCALL.
+// System call number in %eax.
+// Arguments on the stack, from the user call to the C
+// library system call function. The saved user %esp points
+// to a saved program counter, and then the first argument.
+
+// Fetch the int at addr from the current process.
+int
+fetchint(uint addr, int *ip)
+{
+  struct proc *curproc = myproc();
+
+  if(addr >= curproc->sz || addr+4 > curproc->sz)
+    return -1;
+  *ip = *(int*)(addr);
+  return 0;
+}
+
+// Fetch the nul-terminated string at addr from the current process.
+// Doesn't actually copy the string - just sets *pp to point at it.
+// Returns length of string, not including nul.
+int
+fetchstr(uint addr, char **pp)
+{
+  char *s, *ep;
+  struct proc *curproc = myproc();
+
+  if(addr >= curproc->sz)
+    return -1;
+  *pp = (char*)addr;
+  ep = (char*)curproc->sz;
+  for(s = *pp; s < ep; s++){
+    if(*s == 0)
+      return s - *pp;
+  }
+  return -1;
+}
+
+// Fetch the nth 32-bit system call argument.
+int
+argint(int n, int *ip)
+{
+  return fetchint((myproc()->tf->esp) + 4 + 4*n, ip);
+}
+
+// Fetch the nth word-sized system call argument as a pointer
+// to a block of memory of size bytes.  Check that the pointer
+// lies within the process address space.
+int
+argptr(int n, char **pp, int size)
+{
+  int i;
+  struct proc *curproc = myproc();
+ 
+  if(argint(n, &i) < 0)
+    return -1;
+  if(size < 0 || (uint)i >= curproc->sz || (uint)i+size > curproc->sz)
+    return -1;
+  *pp = (char*)i;
+  return 0;
+}
+
+// Fetch the nth word-sized system call argument as a string pointer.
+// Check that the pointer is valid and the string is nul-terminated.
+// (There is no shared writable memory, so the string can't change
+// between this check and being used by the kernel.)
+int
+argstr(int n, char **pp)
+{
+  int addr;
+  if(argint(n, &addr) < 0)
+    return -1;
+  return fetchstr(addr, pp);
+}
+
+extern int sys_chdir(void);
+extern int sys_close(void);
+extern int sys_dup(void);
+extern int sys_exec(void);
+extern int sys_exit(void);
+extern int sys_fork(void);
+extern int sys_fstat(void);
+extern int sys_getpid(void);
+extern int sys_kill(void);
+extern int sys_link(void);
+extern int sys_mkdir(void);
+extern int sys_mknod(void);
+extern int sys_open(void);
+extern int sys_pipe(void);
+extern int sys_read(void);
+extern int sys_sbrk(void);
+extern int sys_sleep(void);
+extern int sys_unlink(void);
+extern int sys_wait(void);
+extern int sys_write(void);
+extern int sys_uptime(void);
+extern int sys_trace(void);
+
+static int (*syscalls[])(void) = {
+[SYS_fork]    sys_fork,
+[SYS_exit]    sys_exit,
+[SYS_wait]    sys_wait,
+[SYS_pipe]    sys_pipe,
+[SYS_read]    sys_read,
+[SYS_kill]    sys_kill,
+[SYS_exec]    sys_exec,
+[SYS_fstat]   sys_fstat,
+[SYS_chdir]   sys_chdir,
+[SYS_dup]     sys_dup,
+[SYS_getpid]  sys_getpid,
+[SYS_sbrk]    sys_sbrk,
+[SYS_sleep]   sys_sleep,
+[SYS_uptime]  sys_uptime,
+[SYS_open]    sys_open,
+[SYS_write]   sys_write,
+[SYS_mknod]   sys_mknod,
+[SYS_unlink]  sys_unlink,
+[SYS_link]    sys_link,
+[SYS_mkdir]   sys_mkdir,
+[SYS_close]   sys_close,
+[SYS_trace]   sys_trace,
+};
+
+static char *syscall_names[] = {
+[SYS_fork]    "fork",
+[SYS_exit]    "exit",
+[SYS_wait]    "wait",
+[SYS_pipe]    "pipe",
+[SYS_read]    "read",
+[SYS_kill]    "kill",
+[SYS_exec]    "exec",
+[SYS_fstat]   "fstat",
+[SYS_chdir]   "chdir",
+[SYS_dup]     "dup",
+[SYS_getpid]  "getpid",
+[SYS_sbrk]    "sbrk",
+[SYS_sleep]   "sleep",
+[SYS_uptime]  "uptime",
+[SYS_open]    "open",
+[SYS_write]   "write",
+[SYS_mknod]   "mknod",
+[SYS_unlink]  "unlink",
+[SYS_link]    "link",
+[SYS_mkdir]   "mkdir",
+[SYS_close]   "close",
+};
+
+void
+syscall(void)
+{
+  int num;
+  struct proc *curproc = myproc();
+
+  num = curproc->tf->eax;
+  if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
+    if(curproc->tracing && strncmp(curproc->name, "trace",5) != 0) {
+      cprintf("[SYSCALL] %s", syscall_names[num]);
+      switch(num) {
+        case SYS_write: {
+          int fd = -1, n = -1;
+          char *buf = 0;
+          argint(0, &fd);
+          argint(2, &n);
+          argptr(1, &buf, n);
+          cprintf("(fd=%d, buf=%p, n=%d)", fd, buf, n);
+          break;
+        }
+        case SYS_read: {
+          int fd = -1, n = -1;
+          char *buf = 0;
+          argint(0, &fd);
+          argint(2, &n);
+          argptr(1, &buf, n);
+          cprintf("(fd=%d, buf=%p, n=%d)", fd, buf, n);
+          break;
+        }
+        case SYS_open: {
+          char *path=0;
+          argstr(0, &path);
+          cprintf("(path=%s)", path);
+          break;
+        }
+        case SYS_kill: {
+          int pid=-1;
+          argint(0, &pid);
+          cprintf("(pid=%d)", pid);
+          break;
+        }
+        case SYS_close: {
+          int fd=-1;
+          argint(0, &fd);
+          cprintf("(fd=%d)", fd);
+          break;
+        }
+        case SYS_dup: {
+          int fd=-1;
+          argint(0, &fd);
+          cprintf("(fd=%d)", fd);
+          break;
+        }
+        case SYS_pipe: {
+          char *p=0;
+          argptr(0, &p, sizeof(int)*2);
+          cprintf("(pipefds)");
+          break;
+        }
+        case SYS_fstat: {
+          int fd=-1;
+          char *statbuf;
+          argint(0, &fd);
+          argptr(1, &statbuf, sizeof(struct stat));
+          cprintf("(fd=%d)", fd);
+          break;
+        }
+        case SYS_unlink: {
+          char *path=0;
+          argstr(0, &path);
+          cprintf("(path=%s)", path);
+          break;
+          }
+        case SYS_link: {
+          char *old=0, *new=0;
+          argstr(0, &old);
+          argstr(1, &new);
+          cprintf("(old=%s,new=%s)", old, new);
+          break;
+        }
+        case SYS_mkdir: {
+          char *path=0;
+          argstr(0, &path);
+          cprintf("(path=%s)", path);
+          break;
+        }
+        case SYS_mknod: {
+          char *path=0;
+          int major=-1, minor=-1;
+          argstr(0, &path);
+          argint(1, &major);
+          argint(2, &minor);
+          cprintf("(path=%s,maj=%d,min=%d)", path, major, minor);
+          break;
+        }
+        case SYS_sbrk: {
+          int n=-1;
+          argint(0, &n);
+          cprintf("(n=%d)", n);
+          break;
+        }
+        case SYS_sleep: {
+          int ticks=-1;
+          argint(0, &ticks);
+          cprintf("(ticks=%d)", ticks);
+          break;
+        }
+        case SYS_exec: {
+          char *path = 0;
+          if(argstr(0, &path) >= 0 && path)  // si se pudo leer el argumento
+              cprintf("(path=%s)", path);
+          break;
+        }
+
+        case SYS_fork:
+        case SYS_exit:
+        case SYS_wait:
+        case SYS_getpid:
+        case SYS_uptime:
+          break;
+        default:
+          break;
+      }
+      cprintf("\n");
+    }
+
+    curproc->tf->eax = syscalls[num]();
+  } else {
+    cprintf("%d %s: unknown sys call %d\n",
+            curproc->pid, curproc->name, num);
+    curproc->tf->eax = -1;
+  }
+}
